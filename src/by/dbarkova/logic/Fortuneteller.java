bc380ab8aa5dd3856df3245e7a64fd5bffec870d
daryabarkova <34532532+daryabarkova@users.noreply.github.com>
2017-12-26 11:27:59 +0300

    Add files via upload

---
+++ src/by/dbarkova/logic/Fortuneteller.java
@@ -0,0 +1,18 @@
+package by.dbarkova.logic;
+
+import by.dbarkova.bean.Flower;
+import by.dbarkova.bean.Leaf;
+
+public class Fortuneteller {
+	
+	public void readFortune(Flower flower){
+	  Leaf[] leavesFlower = flower.getBud().getLeaves();
+	  
+	  for(int i = 0; i < leavesFlower.length; i += 4){
+		 for(int c = 0; c < 4; c++);
+		 
+	  
+	  }
+    }
+
+}
---
+++ src/by/dbarkova/runner/MainApp.java
@@ -0,0 +1,36 @@
+package by.dbarkova.runner;
+
+import by.dbarkova.bean.Flower;
+import by.dbarkova.logic.BotanicEncyclopedia;
+import by.dbarkova.logic.Fortuneteller;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		Flower flower = new Flower("Camomile", 10, 1);
+		
+		BotanicEncyclopedia botanicEncyclopedia = new BotanicEncyclopedia();
+		
+		botanicEncyclopedia.printFlowerInfo(flower);
+		
+		flower.grow(25, 3, 5);
+		botanicEncyclopedia.printFlowerInfo(flower);
+		
+		flower.bloom(30, "Yellow");
+		botanicEncyclopedia.printFlowerInfo(flower);
+		
+		flower.wither(2, "Grey", 20, 2);
+		botanicEncyclopedia.printFlowerInfo(flower);
+		
+		Fortuneteller fortuneteller = new Fortuneteller();
+		
+		Flower flowerFortune = new Flower("Camomile", 10, 1);
+		flowerFortune.bloom(30, "Yellow");
+		
+		fortuneteller.readFortune(flowerFortune);
+		
+
+	}
+
+}
