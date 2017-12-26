f43677bd5630363772a7fd83d4ee124a696cde63
daryabarkova <34532532+daryabarkova@users.noreply.github.com>
2017-12-26 17:10:15 +0300

    Update MainApp.java

--- src/by/dbarkova/runner/MainApp.java
+++ src/by/dbarkova/runner/MainApp.java
@@ -10,25 +10,27 @@ public class MainApp {
 		
 		Flower flower = new Flower("Camomile", 10, 1);
 		
-		BotanicEncyclopedia botanicEncyclopedia = new BotanicEncyclopedia();
+		//BotanicEncyclopedia botanicEncyclopedia = new BotanicEncyclopedia();
 		
-		botanicEncyclopedia.printFlowerInfo(flower);
+		//botanicEncyclopedia.printFlowerInfo(flower);
 		
-		flower.grow(25, 3, 5);
-		botanicEncyclopedia.printFlowerInfo(flower);
+		//flower.grow(25, 3, 5);
+		//botanicEncyclopedia.printFlowerInfo(flower);
 		
 		flower.bloom(30, "Yellow");
-		botanicEncyclopedia.printFlowerInfo(flower);
+		//botanicEncyclopedia.printFlowerInfo(flower);
 		
-		flower.wither(2, "Grey", 20, 2);
-		botanicEncyclopedia.printFlowerInfo(flower);
+		//flower.wither(2, "Grey", 20, 2);
+		//botanicEncyclopedia.printFlowerInfo(flower);
 		
 		Fortuneteller fortuneteller = new Fortuneteller();
 		
-		Flower flowerFortune = new Flower("Camomile", 10, 1);
-		flowerFortune.bloom(30, "Yellow");
+		//Flower flowerFortune = new Flower("Camomile", 10, 1);
+		//flowerFortune.bloom(30, "Yellow");
 		
-		fortuneteller.readFortune(flowerFortune);
+		//fortuneteller.readLoveFortune(flowerFortune);
+		
+		fortuneteller.predictionRequest(3, flower);
 		
 
 	}
